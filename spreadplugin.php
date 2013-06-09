1e6b982e7d020a8d4b862993c414366e70ccbf60
pr3ss-play <pr3ss-play@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-06-09 19:08:42 +0000

    Pagination bugfix: In some cases, the pagination doesn't work and always shows the first page.
    git-svn-id: https://plugins.svn.wordpress.org/wp-spreadplugin/trunk@724706 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- spreadplugin.php
+++ spreadplugin.php
@@ -3,7 +3,7 @@
  * Plugin Name: WP-Spreadplugin
  * Plugin URI: http://wordpress.org/extend/plugins/wp-spreadplugin/
  * Description: This plugin uses the Spreadshirt API to list articles and let your customers order articles of your Spreadshirt shop using Spreadshirt order process.
- * Version: 2.9.3
+ * Version: 2.9.4
  * Author: Thimo Grauerholz
  * Author URI: http://www.pr3ss-play.de
  */
@@ -184,6 +184,11 @@ if(!class_exists('WP_Spreadplugin')) {
 
 				// use pagination value from wordpress
 				if(empty($paged)) $paged = 1;
+				
+				// pagination workaround
+				if (intval($_GET['paged'])>1&&$paged==1) {
+					$paged=intval($_GET['paged']);
+				}
 
 				$offset=($paged-1)*self::$shopOptions['shop_limit'];
 
