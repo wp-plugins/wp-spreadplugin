07589b6ec72f459368e968f2234c2969600fdc51
pr3ss-play <pr3ss-play@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-07-25 11:55:12 +0000

    Missing image Comment in options.php removed - causing an error
    git-svn-id: https://plugins.svn.wordpress.org/wp-spreadplugin/trunk@746251 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- options.php
+++ options.php
@@ -1,16 +1,5 @@
 <?php 
 
-/**
- * Plugin Name: WP-Spreadplugin
- * Plugin URI: http://wordpress.org/extend/plugins/wp-spreadplugin/
- * Description: This plugin uses the Spreadshirt API to list articles and let your customers order articles of your Spreadshirt shop using Spreadshirt order process.
- * Version: 3.0
- * Author: Thimo Grauerholz
- * Author URI: http://lovetee.de/
- */
-
-
-
 if (is_user_logged_in() && is_admin()) {
 	
 	$adminSettings = $this->defaultOptions;
