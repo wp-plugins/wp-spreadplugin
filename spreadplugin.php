04a15a7b726574277c57e59e163f9c37c9f38cc4
pr3ss-play <pr3ss-play@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-05-04 17:29:19 +0000

    removed the caching block, didn't solve the problem
    git-svn-id: https://plugins.svn.wordpress.org/wp-spreadplugin/trunk@1153115 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- spreadplugin.php
+++ spreadplugin.php
@@ -3,12 +3,11 @@
  * Plugin Name: WP-Spreadplugin
  * Plugin URI: http://wordpress.org/extend/plugins/wp-spreadplugin/
  * Description: This plugin uses the Spreadshirt API to list articles and let your customers order articles of your Spreadshirt shop using Spreadshirt order process.
- * Version: 3.8.6.3
+ * Version: 3.8.6.4
  * Author: Thimo Grauerholz
  * Author URI: http://www.spreadplugin.de
  */
 @set_time_limit(0);
-@define('DONOTCACHEPAGE', true);
 
 /**
  * WP_Spreadplugin class
