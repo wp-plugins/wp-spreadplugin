23009f97bb3152891854fb3522b52fe6d8b87ce4
pr3ss-play <pr3ss-play@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-07-16 20:55:20 +0000

    Bugfix
    git-svn-id: https://plugins.svn.wordpress.org/wp-spreadplugin/trunk@1200499 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- spreadplugin.php
+++ spreadplugin.php
@@ -3,7 +3,7 @@
  * Plugin Name: WP-Spreadplugin
  * Plugin URI: http://wordpress.org/extend/plugins/wp-spreadplugin/
  * Description: This plugin uses the Spreadshirt API to list articles and let your customers order articles of your Spreadshirt shop using Spreadshirt order process.
- * Version: 3.8.7.5
+ * Version: 3.8.7.6
  * Author: Thimo Grauerholz
  * Author URI: http://www.spreadplugin.de
  */
